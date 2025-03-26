aeedfe493195bac35a529dc9420624c0cf66aef5
Lisandro <lisandroprieri@gmail.com>
2025-03-26 15:42:07 +0100

    Added a matrix variable in t_objects, and renamed the transform variable to direction.

--- include/scene.h
+++ include/scene.h
@@ -74,7 +74,8 @@ typedef struct s_object
 {
 	t_object_type	type;
 	t_tuple			position;
-	t_tuple			transform;
+	t_matrix		transform;
+	t_tuple			direction;
 	t_color			color;
 	double			diameter;
 	double			height;
