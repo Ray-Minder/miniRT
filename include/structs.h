bc584f9efa79225126a9dcf59faa6c87246e134d
Elroy Andela <elroyandela@gmail.com>
2025-03-31 12:53:16 +0200

    change colors in objects from uint8 to double

--- include/structs.h
+++ include/structs.h
@@ -25,11 +25,18 @@ typedef enum e_object_type
 	CYLINDER
 }	t_object_type;
 
-typedef struct s_color
+typedef struct s_uint8color
 {
 	uint8_t	r;
 	uint8_t g;
 	uint8_t b;
+}	t_uint8color;
+
+typedef struct s_color
+{
+	double	r;
+	double	g;
+	double	b;
 }	t_color;
 
 typedef struct s_ambient_light
--- src/parsing/parse_uint8.c
+++ src/parsing/parse_uint8.c
@@ -25,6 +25,7 @@ int	parse_color(char *line, t_color *color)
 {
 	char	**split_line;
 	int		error;
+	t_uint8color	uint8color;
 
 	split_line = ft_split(line, ',');
 	if (!split_line)
@@ -34,14 +35,17 @@ int	parse_color(char *line, t_color *color)
 		ft_free_ptrs(split_line);
 		return (INVALID_ARG_COUNT);
 	}
-	error = parse_uint8(split_line[0], &color->r);
-	error += parse_uint8(split_line[1], &color->g);
-	error += parse_uint8(split_line[2], &color->b);
+	error = parse_uint8(split_line[0], &uint8color.r);
+	error += parse_uint8(split_line[1], &uint8color.g);
+	error += parse_uint8(split_line[2], &uint8color.b);
 	if (error != SUCCESS)
 	{
 		ft_free_ptrs(split_line);
 		return (INVALID_INT);
 	}
+	color->r = (double)uint8color.r / 255.0;
+	color->g = (double)uint8color.g / 255.0;
+	color->b = (double)uint8color.b / 255.0;
 	ft_free_ptrs(split_line);
 	return (SUCCESS);
 }
