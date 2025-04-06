d2c848f2d1ae6741cac916bd470f76b9c2cb3c58
Lisandro <lisandroprieri@gmail.com>
2025-04-06 19:52:13 +0200

    Render scene now renders all spheres, like it did before we started refactoring, however this is computationally very expensive.

--- src/rendering/render_scene.c
+++ src/rendering/render_scene.c
@@ -7,11 +7,13 @@ void render_scene(t_scene *scene)
 	t_x				*xs_list;
 	t_x				*xs;
 	t_x				*_hit;
-	
+
 	t_render_params params;
 	t_comps			comps;
 
 	t_object *sphere;
+	t_object *head;
+	t_object *current;
 
 	int			y;
 	int			x;
@@ -25,50 +27,53 @@ void render_scene(t_scene *scene)
 	xs = NULL;
 	_hit = NULL;
 
-	sphere = scene->objects;
+	head = scene->objects;
+	sphere = head;
+	current = head;
+	
 	// print_objects(sphere);
-	y = -1;
-	while (++y < data.height)
+	while (current)
 	{
-		world_y = params.half_wall - params.pixel_size * y;
-		x = -1;
-		// printf("y: %d\n", y);
-		while (++x < data.width)
+
+		y = -1;
+		while (++y < data.height)
 		{
-			xs = NULL;
-			_hit = NULL;
-			world_x = -params.half_wall + params.pixel_size * x;
-			r_position = point(world_x, world_y, params.wall_z);
-			params.ray.direction = normalize_tuple(subtract_tuples(r_position, params.ray.origin));
-			printf("y: %d, x: %d\n", y, x);
-
-			while(sphere)
+			world_y = params.half_wall - params.pixel_size * y;
+			x = -1;
+			// printf("y: %d\n", y);
+			while (++x < data.width)
 			{
-				xs = intersect(&params.ray, sphere);
-				if (xs)
-					add_intersection_node(&xs_list, xs);				
-				sphere = sphere->next;
+				sphere = current;
+				xs = NULL;
+				_hit = NULL;
+				world_x = -params.half_wall + params.pixel_size * x;
+				r_position = point(world_x, world_y, params.wall_z);
+				params.ray.direction = normalize_tuple(subtract_tuples(r_position, params.ray.origin));
+				printf("y: %d, x: %d\n", y, x);
+
+				while (sphere)
+				{
+					xs = intersect(&params.ray, sphere);
+					if (xs)
+						add_intersection_node(&xs_list, xs);
+					sphere = sphere->next;
+				}
+				_hit = hit(xs_list);
+				if (_hit && _hit->hit)
+				{
+					t_color final_color;
+
+					prepare_computations(&comps, _hit, &params.ray);
+					final_color = lighting(scene, &comps);
+					mlx_put_pixel(data.canvas, x, y, color_to_uint32(final_color));
+				}
+
+				free_intersections_list(&xs_list);
 			}
-
-			printf("xs: %p\n", xs);
-			_hit = hit(xs_list);
-			if (_hit && _hit->hit)
-			{
-				t_color final_color;
-
-				prepare_computations(&comps, _hit, &params.ray);
-				final_color = lighting(scene, &comps);
-				mlx_put_pixel(data.canvas, x, y, color_to_uint32(final_color));
-			}
-
-			free_intersections_list(&xs_list);
-			if (scene->objects)
-				scene->objects = scene->objects->next;
-			sphere = scene->objects;
+			// print_intersection_list(xs_list);
 		}
-		// print_intersection_list(xs_list);
+		current = current->next;
 	}
-
 	if (mlx_image_to_window(data.mlx, data.canvas, 0, 0) < 0)
 	{
 		printf("Failure to put image to window\n");
