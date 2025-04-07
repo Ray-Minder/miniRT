b1ab3c320a8d22cd7015c68d065c92241c01b432
Lisandro <lisandroprieri@gmail.com>
2025-04-07 18:19:15 +0200

    modified the camera to comply with norm

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -7,15 +7,10 @@ t_camera *camera(double hsize, double vsize, double field_of_view)
 	cam = ft_calloc(1, sizeof(t_camera));
 	if (!cam)
 		return (NULL);
-	cam->hsize = hsize;
-	cam->vsize = vsize;
 	cam->fov = field_of_view;
 	cam->transform = identity_matrix(4);
 	if (!cam->transform)
-	{
-		free(cam);
-		return (NULL);
-	}
+		return (free(cam), NULL);
 	cam->half_view = tan(field_of_view / 2);
 	cam->aspect = hsize / vsize;
 	if (cam->aspect >= 1)
@@ -28,6 +23,6 @@ t_camera *camera(double hsize, double vsize, double field_of_view)
 		cam->half_height = cam->half_view;
 		cam->half_width = cam->half_view * cam->aspect;
 	}
-	cam->pixel_size = (cam->half_width * 2) / cam->hsize;
+	cam->pixel_size = (cam->half_width * 2) / hsize;
 	return (cam);
 }
