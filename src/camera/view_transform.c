ce8b956fed1306da03f402d801136504b1a7a6fd
Lisandro <lisandroprieri@gmail.com>
2025-04-16 17:21:39 +0200

    Changes on view_transform

--- src/camera/view_transform.c
+++ src/camera/view_transform.c
@@ -1,5 +1,19 @@
 #include "../../include/minirt.h"
 
+static void	set_orientation_values(t_matrix *orientation, t_tuple right,
+	t_tuple up, t_tuple forward)
+{
+	orientation->values[0][0] = right.x;
+	orientation->values[0][1] = right.y;
+	orientation->values[0][2] = right.z;
+	orientation->values[1][0] = up.x;
+	orientation->values[1][1] = up.y;
+	orientation->values[1][2] = up.z;
+	orientation->values[2][0] = -forward.x;
+	orientation->values[2][1] = -forward.y;
+	orientation->values[2][2] = -forward.z;
+}
+
 /**
  * @brief This function computes the camera's view matrix.
  * 
@@ -19,6 +33,7 @@ t_matrix	*view_transform(t_tuple forward, t_tuple point)
 	t_tuple		right;
 	t_tuple		up;
 	t_matrix	*orientation;
+	t_matrix	*translation_m;
 	t_matrix	*_view_transform;
 
 	right = normalize_tuple(get_camera_right(forward));
@@ -26,16 +41,17 @@ t_matrix	*view_transform(t_tuple forward, t_tuple point)
 	orientation = identity(4);
 	if (!orientation)
 		return (NULL);
-	orientation->values[0][0] = right.x;
-	orientation->values[0][1] = right.y;
-	orientation->values[0][2] = right.z;
-	orientation->values[1][0] = up.x;
-	orientation->values[1][1] = up.y;
-	orientation->values[1][2] = up.z;
-	orientation->values[2][0] = -forward.x;
-	orientation->values[2][1] = -forward.y;
-	orientation->values[2][2] = -forward.z;
-	_view_transform = multiply_matrices(orientation, translation(-point.x, -point.y, -point.z));
+	set_orientation_values(orientation, right, up, forward);
+	translation_m = translation(-point.x, -point.y, -point.z);
+	if (!translation_m)
+	{
+		free_matrix(&orientation);
+		return (NULL);
+	}
+	_view_transform = multiply_matrices(orientation, translation_m);
 	free_matrix(&orientation);
+	free_matrix(&translation_m);
+	if (!_view_transform)
+		return (NULL);
 	return (_view_transform);
 }
