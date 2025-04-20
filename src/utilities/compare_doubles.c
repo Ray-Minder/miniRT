5be44021fa2e13e0118af27bdbefc7ed93720e28
Lisandro <lisandroprieri@gmail.com>
2025-04-20 18:03:46 +0200

    Add commens to radians and compare_doubles functions

--- src/utilities/compare_doubles.c
+++ src/utilities/compare_doubles.c
@@ -1,5 +1,16 @@
 #include "../../include/minirt.h"
 
+/**
+ * @brief Compares two double values with a small epsilon value
+ * 		to account for floating point precision.
+ * 
+ * @param a The first double value to compare.
+ * @param b The second double value to compare.
+ * 
+ * @return true if the two values are considered equal, false otherwise.
+ * 
+ * @note The EPSILON value is defined as 1e-6.
+ */
 bool compare_doubles(double a, double b)
 {
 	if (a - b < EPSILON && a - b > -EPSILON)
