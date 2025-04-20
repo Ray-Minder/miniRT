608c34d2066d87e474100473df1c64d7419c7599
Lisandro <lisandroprieri@gmail.com>
2025-04-20 18:12:10 +0200

    Remove free_light function since there's nothing allocated in the light structure

--- src/errors/clean.c
+++ src/errors/clean.c
@@ -5,7 +5,6 @@
 void	clean_and_exit(t_data *data, int exit_code);
 void	free_scene(t_scene **scene);
 void	free_camera(t_camera *camera);
-void	free_light(t_light *light);
 void	free_objects(t_object **object_list);
 
 //	=== Function Definitions ===
@@ -33,7 +32,7 @@ void	free_scene(t_scene **scene)
 	if ((*scene)->camera.is_set)
 		free_camera(&(*scene)->camera);
 	if ((*scene)->light.is_set)
-		free_light(&(*scene)->light);
+		(*scene)->light.is_set = false;
 	if ((*scene)->objects != NULL)
 		free_objects(&(*scene)->objects);
 	free(*scene);
@@ -50,13 +49,6 @@ void	free_camera(t_camera *camera)
 	if (camera->inverse_transform)
 		free_matrix(&camera->inverse_transform);
 	camera->is_set = false;
-}
-
-void	free_light(t_light *light)
-{
-	if (!light)
-		return ;
-	light->is_set = false;
 }
 
 void	free_objects(t_object **object_list)
